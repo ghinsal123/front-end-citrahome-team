3127876d1ae0573df47fa4d8f0d280d3bc72d8cd
Putri Aisyah <dwijanti9@gmail.com>
2025-07-03 15:07:12 +0700

    menambahkan product card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import TextField from "../components/TextField";
+import ProductCard from "../components/ProductCard";
 
 export default function Home() {
   return (
     <div className="w-full min-h-screen p-6 space-y-10 bg-white">
+
       <section>
         <h2 className="text-xl text-gray-800 font-bold mb-4">Preview TextField</h2>
-
-        {/* kalau mau kotak contoh punya latar tersendiri, bisa tambahkan bg-slate-100 */}
         <div className="space-y-8">
-          <TextField placeholder="Contoh Input" />
+        <TextField placeholder="Contoh Input" />
         </div>
       </section>
+
+      <section>
+        <h2 className="font-bold text-gray-800 mb-2">Preview Product Card 1</h2>
+        <ProductCard />
+      </section>
     </div>
+    
   );
 }
