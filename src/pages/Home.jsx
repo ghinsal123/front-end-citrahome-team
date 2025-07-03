d417a40017e9430f0cc68f157eb4d719b1044bc9
Putri Aisyah <dwijanti9@gmail.com>
2025-07-03 14:14:45 +0700

    style components text field

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import TextField from "../components/TextField";
 
-const Home = () => {
+export default function Home() {
     return(
-        <div className="bg-white min-h-screen px-4 py-6 space-y-8">
+        <div className="bg-white max-w-7xl mx-auto p-6 space-y-10">
 
-            {/* Input Pencarian */}
-            <section>
-
-
-            </section>
+      {/* Input Pencarian */}
+      <section>
+        <h2 className="font-bold text-gray-800 mb-2">Preview Text Field</h2>
+        <TextField placeholder="Placeholder" />
+      </section>
         </div>
-    )
-}
+    );
+};
 
-export default Home;
