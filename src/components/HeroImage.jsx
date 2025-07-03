f701fa7d7bc4fc887ca13691008e680c3f62ad1e
ghinsal123 <gsalsabila570@gmail.com>
2025-07-03 16:16:54 +0700

    menambahkan komponen HeroImage

---
+++ src/components/HeroImage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function HeroImage() {
+  return (
+    <div className="relative w-full max-w-5xl mx-auto overflow-hidden rounded-2xl shadow-lg group">
+      <img
+        src="/img/hero.jpg" // atau import dari assets
+        alt="Hero"
+        className="w-full h-[400px] md:h-[600px] object-cover transition-transform duration-500 group-hover:scale-105"
+      />
+
+      <div className="absolute inset-0 bg-black/40 transition-opacity duration-500 group-hover:opacity-0" />
+    </div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import Best from "../components/Best";
 import ShopeeButton from "../components/ShopeeButton";
 import TokopediaButton from "../components/TokopediaButton";
 import BuyButton from "../components/BuyButton";
-
+import HeroImage from '../components/HeroImage'
 
 
 export default function Home() {
@@ -20,25 +20,25 @@ export default function Home() {
         </div>
       </section>
       <section>
-        <h2 className="text-xl text-gray-800 font-bold mb-4">Preview TextField</h2>
+        <h2 className="text-xl text-gray-800 font-bold mb-4">Preview AllProduct</h2>
         <div className="space-y-6">
           <AllProduct />
         </div>
       </section>
       <section>
-        <h2 className="text-xl text-gray-800 font-bold mb-4">Preview TextField</h2>
+        <h2 className="text-xl text-gray-800 font-bold mb-4">Preview ProductCard</h2>
         <div className="space-y-6">
           <ProductCard />
         </div>
       </section>
       <section>
-        <h2 className="text-xl text-gray-800 font-bold mb-4">Preview TextField</h2>
+        <h2 className="text-xl text-gray-800 font-bold mb-4">Preview Popular</h2>
         <div className="space-y-6 max-w-xs">
           <Popular src="/img/popular1.jpg" />
         </div>
       </section>
       <section>
-        <h2 className="text-xl text-gray-800 font-bold mb-4">Preview TextField</h2>
+        <h2 className="text-xl text-gray-800 font-bold mb-4">Preview Best</h2>
         <div className="space-y-6 max-w-xs">
           <Best src="/img/best1.jpg" />
         </div>
@@ -62,6 +62,12 @@ export default function Home() {
         <div>
         <BuyButton />
         </div>
+        </section>
+        <section>
+          <h2 className="text-xl text-gray-800 font-bold mb-4">Preview Hero Image</h2>
+          <div className="p-6 space-y-6">
+            <HeroImage />
+          </div>
         </section>
     </div>
   );
