832a9d9b03941b201cb501535aa69e4eca7a9cbb
Putri Aisyah <dwijanti9@gmail.com>
2025-07-06 19:57:16 +0700

    Memperbaiki komponen maps

--- src/components/MapsOp2.jsx
+++ src/components/MapsOp2.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function MapsOp2() {
   return (
-    <div className="w-[520px] h-[240px] rounded-xl overflow-hidden shadow-lg transition duration-300 hover:brightness-75">
+    <div className="w-[520px] h-[240px] rounded-xl overflow-hidden shadow-lg transition duration-300 filter brightness-75 hover:brightness-100">
       <iframe
         src="https://www.google.com/maps?q=-6.2494068,106.9403638&z=15&output=embed"
         className="w-full h-full"
