86da453ef4a53ef3b36d97b8685fcd2745fb4d68
ghinsal123 <gsalsabila570@gmail.com>
2025-07-07 15:39:56 +0700

    memrubah tipe color di file index.css

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -26,7 +26,7 @@ export default function ProductCard () {
       <div className="flex gap-2">
         <a
           href="#"
-          className="bg-[#F17157] hover:bg-[#EE4D2D] text-white text-sm font-semibold px-3 py-1 rounded-md flex items-center gap-1"
+          className="bg-primary hover:bg-[#EE4D2D] text-white text-sm font-semibold px-3 py-1 rounded-md flex items-center gap-1"
         >
           <img src={shopeeIcon} alt="Shopee" className="w-4 h-4" />
           shopee
